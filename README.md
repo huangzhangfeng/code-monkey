8853dfdd59cf6f9271ebee4dec212129a5dbf3ac
Sheki Lyu <lvxueji@gmail.com>
2018-05-08 18:57:21 +0800

    edit readme and finish the two-eight

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # code-monkey
+## 介绍
+本代码库是我学习各种量化策略的源码实现以及心得体会。代码采用了RQalpha框架进行回测，有关RQalpha的内容请参考[https://github.com/ricequant/rqalpha](https://github.com/ricequant/rqalpha)
+
+## 目录结构
+每个目录就是一种量化策略，目录里包含以下几个文件：
+- README.MD 策略相关介绍分析
+- 目录名.py 策略源码主代码文件
+- config.yml 策略快速验证的配置文件
+- backtest.py 策略完整回测的测试代码
+
+## 使用方法
+### 准备工作
+按照RQalpha的指导配置好RQalpha并更新RQalpha的回测数据存放在默认位置
+### 快速验证
+在相应策略目录下执行
+> rqalpha run
+
+### 完整验证
+在相应策略目录下执行
+> python ./backtest.py
+
--- two-eight/two_eight.py
+++ two-eight/two_eight.py
@@ -14,37 +14,35 @@ def init(context):
     logger.info("init")
     context.stocks = STOCKS
     update_universe(context.stocks)
-    context.trading = None
     context.hold = None
     scheduler.run_weekly(handle_bar_weekly, 4)
 
 
 def handle_bar_weekly(context, bar_dict):
+    '''
+    每周运行一次的交易函数
+    '''
     s1 = history_bars(context.stocks[0], 21, '1d','close')
     s2 = history_bars(context.stocks[1], 21, '1d','close')
     s1delta = (s1[-1] - s1[0]) / s1[0]
     s2delta = (s2[-1] - s2[0]) / s2[0]
     log_str = '沪深300涨幅: ' + str(round(s1delta, 2)) + ' 中证500涨幅: ' + str(round(s2delta, 2))
-    hold = None
+    trading = None
     if s1delta is not None and s2delta is not None:
         if s1delta < 0 and s2delta < 0:
-            hold = context.stocks[2]
+            trading = context.stocks[2]
         elif s1delta > s2delta:
-            hold = context.stocks[0]
+            trading = context.stocks[0]
         else:
-            hold = context.stocks[1]
-    if hold is not None:
-        context.trading = hold
-    else:
-        context.trading = context.hold
-    if context.trading is None or context.trading == context.hold:
+            trading = context.stocks[1]
+    if trading is None or trading == context.hold:
         return
     if context.hold is not None:
         order_target_percent(context.hold, 0)
         log_str += ' sell ' + context.hold
-    order_target_percent(context.trading, 1)
-    log_str += ' buy ' + context.trading
-    context.hold = context.trading
+    order_target_percent(trading, 1)
+    log_str += ' buy ' + trading
+    context.hold = trading
     logger.info(log_str)
 
 def before_trading(context):
