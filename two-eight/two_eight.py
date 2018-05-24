32f944c27949b9ec462b0e5addc05b1560688787
Sheki Lyu <lvxueji@gmail.com>
2018-05-24 19:39:35 +0800

    将主策略改成场内交易

--- two-eight/two_eight.py
+++ two-eight/two_eight.py
@@ -15,51 +15,15 @@ def init(context):
     context.stocks = STOCKS
     #当前持仓品种，取值为STOCKS里的值以及cash
     context.hold = 'cash'
-    #下一步调仓品种，取值范围同hold，取值为None时表示持仓不调
-    context.trade = None
     #趋势判断窗口，往前看20个交易日
     context.window_size = 20
-    #记录最近一次的调仓日期
-    #大多数基金存在7日内赎回1.5%的交易手续费
-    #所以两次调仓间隔大于7天才调仓
-    context.trade_date = None
-    #场外交易没有滑点
-    context.slippage = 0
-
-def fund_trading(context):
-    '''
-    基金交易函数，T日发出交易信号，T+1日卖出，T+2日买入
-    '''
-    if context.trade is None:
-        return
-    log_str = '[' + context.now.isoformat() + ']'
-    if context.hold != 'cash' and context.trade == 'cash':
-        order_target_percent(context.hold, 0)
-        log_str += 'sell ' + context.hold
-        context.hold = 'cash'
-        context.trade = None
-    elif context.hold == 'cash' and context.trade != 'cash':
-        order_target_percent(context.trade, 0.99)
-        log_str += 'buy ' + context.trade
-        context.hold = context.trade
-        context.trade = None
-    elif context.hold != 'cash' and context.trade != 'cash':
-        order_target_percent(context.hold, 0)
-        log_str += 'sell ' + context.hold
-        context.hold = 'cash'
-    else:
-        log_str += 'unkown trading signal'
-        logger.error(log_str)
-        return
-    logger.info(log_str)
+    #滑点
+    context.slippage = 0.5
 
 def handle_bar(context, bar_dict):
     '''
     交易函数
     '''
-    if context.trade is not None:
-        fund_trading(context)
-        return
     hist_s1 = history_bars(context.stocks[0],
                            context.window_size + 1, '1d', 'close')
     hist_s2 = history_bars(context.stocks[1],
@@ -85,10 +49,11 @@ def handle_bar(context, bar_dict):
             trading = context.stocks[1]
     if trading is None or trading == context.hold:
         return
-    if context.trade_date is not None:
-        holding_days = context.now - context.trade_date
-        if holding_days.days < 8:
-            return
-    context.trade = trading
-    context.trade_date = context.now
+    if context.hold != 'cash':
+        order_target_percent(context.hold, 0)
+        log_str += 'sell ' + context.hold
+    if trading != 'cash':
+        order_target_percent(trading, 0.99)
+        log_str += 'buy ' + trading
+    context.hold = trading
     logger.info(log_str)
