b4827db44a1e2086150b69b8e6ba62b9ac0e1013
Sheki Lyu <lvxueji@gmail.com>
2018-06-06 16:27:20 +0800

    开始写估值部分

--- two-eight/research/two_eight_pricing/two_eight_pricing.py
+++ two-eight/research/two_eight_pricing/two_eight_pricing.py
@@ -3,9 +3,13 @@
 '''
 # -*- coding: utf-8 -*-
 
-from rqalpha.api import update_universe, logger, order_target_percent, history_bars
+from rqalpha.api import (
+    logger,
+    order_target_percent,
+    history_bars,
+)
 
-STOCKS = ['505888.XSHG', '000012.XSHG']
+STOCKS = ['000300.XSHG', '000905.XSHG']
 
 def init(context):
     '''
@@ -13,21 +17,30 @@ def init(context):
     '''
     logger.info("init")
     context.stocks = STOCKS
-    update_universe(context.stocks)
-    context.hold = None
+    #当前持仓品种，取值为STOCKS里的值以及cash
+    context.hold = 'cash'
+    #趋势判断窗口，往前看20个交易日
+    context.window_size = 20
 
 def handle_bar(context, bar_dict):
     '''
     交易函数
     '''
-    s1 = history_bars(context.stocks[0], 21, '1d','close')
-    s2 = history_bars(context.stocks[1], 21, '1d','close')
-    logger.debug('s1:' + str(s1))
-    logger.debug('s2:' + str(s2))
-    import pdb;pdb.set_trace()
-    s1delta = (s1[-1] - s1[0]) / s1[0]
-    s2delta = (s2[-1] - s2[0]) / s2[0]
-    log_str = '元和 ' + str(s1[0]) + '->' + str(s1[-1]) + ' 涨幅: ' + str(round(s1delta, 3)) + ' 债券 '+ str(s2[0]) + '->' + str(s2[-1]) +  ' 涨幅: ' + str(round(s2delta, 3))
+    hist_s1 = history_bars(context.stocks[0],
+                           context.window_size + 1, '1d', 'close')
+    hist_s2 = history_bars(context.stocks[1],
+                           context.window_size + 1, '1d', 'close')
+    curr_s1 = bar_dict[context.stocks[0]].close
+    curr_s2 = bar_dict[context.stocks[1]].close
+    logger.debug('s1:' + str(hist_s1))
+    logger.debug('s2:' + str(hist_s2))
+    s1delta = (curr_s1  - hist_s1[0]) / hist_s1[0]
+    s2delta = (curr_s2  - hist_s2[0]) / hist_s2[0]
+    log_str = '[' + context.now.isoformat() + ']'
+    log_str += '沪深300 ' + str(hist_s1[0]) + '->' + str(curr_s1)
+    log_str += ' 涨幅: ' + str(round(s1delta, 3))
+    log_str += ' 中证500 '+ str(hist_s2[0]) + '->' + str(curr_s2)
+    log_str += ' 涨幅: ' + str(round(s2delta, 3))
     trading = None
     if s1delta is not None and s2delta is not None:
         if s1delta < 0 and s2delta < 0:
@@ -38,11 +51,11 @@ def handle_bar(context, bar_dict):
             trading = context.stocks[1]
     if trading is None or trading == context.hold:
         return
-    if context.hold is not None and context.hold != 'cash':
+    if context.hold != 'cash':
         order_target_percent(context.hold, 0)
         log_str += ' sell ' + context.hold
     if trading != 'cash':
-        order_target_percent(trading, 1)
+        order_target_percent(trading, 0.99)
         log_str += ' buy ' + trading
     context.hold = trading
     logger.info(log_str)
